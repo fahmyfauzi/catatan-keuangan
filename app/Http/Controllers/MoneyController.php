88726c33c06d37599ee94a5fd44b7d24deec00c1
fahmyfauzi <fahmyfauzii@gmail.com>
2023-01-08 12:23:08 +0700

    create

--- app/Http/Controllers/MoneyController.php
+++ app/Http/Controllers/MoneyController.php
@@ -4,15 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Models\Money;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MoneyController extends Controller
 {
     public function index()
     {
-        $money = Money::latest()->get();
+        $money = Money::latest()->paginate(8);
 
         return view('pages.index', [
             'moneys' => $money
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'keterangan' => 'required',
+            'jumlah' => 'required',
+            'jenis' => 'required',
+        ]);
+
+        //cek validasi salah
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        //create store
+        $money = Money::create([
+            'keterangan' => $request->keterangan,
+            'jumlah' => $request->jumlah,
+            'jenis' => $request->jenis,
+        ]);
+        //berhasil
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Berhasil Ditambahkan!',
+            'data' => $money
         ]);
     }
 }
